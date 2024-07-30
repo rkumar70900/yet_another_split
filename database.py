350a832b5d7670c1cc5ba9bdb6ab114529443bd4
Rajesh Kumar Bandaru <54278302+rkumar70900@users.noreply.github.com>
2024-07-29 20:28:42 -0500

    removed hardcoded password for the database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-password = "2aoySvGG=18!s9oNE}PB"
+password = ""
 engine = create_engine(f"mysql+mysqlconnector://root:{password}@localhost/splitwise_api")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
