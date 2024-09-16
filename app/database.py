e9117ede7ebeb6a60bf0d7f30146bfe7c40822da
Rajesh Kumar Bandaru <54278302+rkumar70900@users.noreply.github.com>
2024-09-16 18:43:16 -0500

    removed password for database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-password = "casaos"
+password = ""
 engine = create_engine(f"mysql+mysqlconnector://root:{password}@localhost/yet_another_split")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
